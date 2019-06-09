120e523cdec4d2c6eff4a52992221968ecb466be
Hassan Abusalih <nichaes1721@gmail.com>
2019-06-09 01:21:23 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,8 @@
 function basicTeenager(age) {
-  if (age > 13) {
+  if (age < 13) {
   return "You are a teenager!"
 }
-  if (age < 19) {
+  if (age > 19) {
     return "You are a teenager!"
   }
 }
